1c3230845a8373533c39f6aac4ccb9ee5e010498
Nayar Joolfoo <nayar@joolfoo.com>
2025-10-30 19:39:08 +0400

    vercel deploy

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>Hello to making my website public from React</h1>
       <h2>I am very excited to share this with the world</h2>
+      <h3>This app is now deployed on vercel</h3>
     </div>
   );
 }
